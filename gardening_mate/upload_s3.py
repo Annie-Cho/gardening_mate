e97cc79eb2c69c98fc02d34d734dd3b315ca71cd
Annie-Cho <ani100482@gmail.com>
2025-07-20 16:00:13 +0900

    Feat: AWS 시크릿 임의로 정의

--- gardening_mate/upload_s3.py
+++ gardening_mate/upload_s3.py
@@ -1,5 +1,7 @@
 import boto3
 
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
 S3_BUCKET_NAME = "lab-s3-webhosting-13459"
 FILE_NAME = "20250717_16h42m33s.csv"
 
@@ -10,9 +12,6 @@ s3_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name="ap-northeast-2"
 )
-
-# buckets = s3_client.list_buckets()
-# print(buckets)
 
 try:
     # upload file
